47a0502e96558f9377a6bdf5a4cfd109871ee18e
Swifter-n <54946777+Swifter-n@users.noreply.github.com>
2020-04-27 22:36:45 +0700

    Add files via upload
    add

---
+++ core/init.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+require_once "function/config.php";
+require_once "function/users.php";
+
+ ?>
---
+++ file/sales.php
@@ -0,0 +1,29 @@
+
+
+<h1 class="text-center">How To Use <br/>Inventory Management Systems</h1>
+<div class="embed-responsive embed-responsive-16by9 text-center">
+<video controls="controls" autoplay="autoplay" height="200px" width="300px" class="embed-responsive-item" >
+  <source src="video/sales rev.mp4" type="video/mp4"/>
+</video>
+</div>
+
+
+  <div class="row">
+    <div class="col-md-11">
+<h3>Description</h3>
+</div>
+<div class="col-md-1">
+  <a href="https://drive.google.com/file/d/0B0Sc5Qs-YiMVdTI1Y3FHbUxNZ2c/view?usp=sharing" target="_blank"><img src="images/DL.png"></a></div>
+<div class="col-md-12">
+<div class="panel panel-primary">
+  <div class="panel-heading">Sales Video</div>
+  <div class="panel-body">
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+       but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages
+       , and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  </div>
+</div>
+
+</div>
+</div>
